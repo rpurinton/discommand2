0e62c491f534e19b9f6c22bb9ad322184c03ea17
Russell Purinton <russell.purinton@icloud.com>
2023-11-17 15:00:20 +0000

    update

--- src/BunnyConsumer.php
+++ src/BunnyConsumer.php
@@ -56,7 +56,7 @@ class BunnyConsumer extends ConfigLoader
 			1 => array("pipe", "w"),
 			2 => array("pipe", "w")
 		);
-		$process = proc_open(__DIR__ . "/BunnyPublisher.php '$queue'", $descriptorspec, $pipes);
+		$process = proc_open("/usr/local/bin/php -f " . __DIR__ . "/BunnyPublisher.php '$queue'", $descriptorspec, $pipes);
 		if (is_resource($process)) {
 			fwrite($pipes[0], $json_string);
 			fclose($pipes[0]);
