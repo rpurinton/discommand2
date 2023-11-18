480a016483ecb39b6da0a42309f1ddae917e58e7
Russell Purinton <russell.purinton@gmail.com>
2023-11-18 04:22:08 -0500

    Update ConfigLoader.php

--- src/ConfigLoader.php
+++ src/ConfigLoader.php
@@ -10,7 +10,11 @@ class ConfigLoader
 
     public function __construct()
     {
-        // Existing constructor code...
+        try {
+            foreach (glob(__DIR__ . "/../conf.d/*.json") as $configFile) $this->config[basename($configFile, '.json')] = json_decode(file_get_contents($configFile), true);
+        } catch (\Throwable $e) {
+            throw new ConfigurationException("Failed to load configuration: {$e->getMessage()}");
+        }
     }
 
     // Intentionally trigger a ConfigurationException
