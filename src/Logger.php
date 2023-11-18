05e9e2140450101b828913cf2382bceed363b850
Russell Purinton <russell.purinton@gmail.com>
2023-11-17 22:50:26 -0500

    Update Logger.php

--- src/Logger.php
+++ src/Logger.php
@@ -8,9 +8,7 @@ class Logger
 
     public static function log($message)
     {
-        if (!is_dir(self::$log_dir)) {
-            mkdir(self::$log_dir, 0777, true);
-        }
+        if (!is_dir(self::$log_dir)) mkdir(self::$log_dir, 0777, true);
 
         $log_file = self::$log_dir . '/' . date('Y-m-d') . '.log';
         $timestamp = "[" . date('Y-m-d H:i:s') . '.' . explode(' ', microtime())[0] . ']';
