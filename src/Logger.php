404a98fa796024a1e65c4b3ddf589edc7c13e8f8
Russell Purinton <russell.purinton@gmail.com>
2023-11-17 22:39:48 -0500

    Update Logger.php

--- src/Logger.php
+++ src/Logger.php
@@ -6,6 +6,10 @@ class Logger
 {
     public static function log($message)
     {
-        error_log($message . "\n", 3, __DIR__ . '/../logs/app.log');
+        echo ($message . "\n");
+        $log_dir = __DIR__ . '/../logs.d';
+        if (!is_dir($log_dir)) mkdir($log_dir);
+        $log_file = $log_dir . '/' . date('Y-m-d') . '.log';
+        file_put_contents($log_file, date('Y-m-d H:i:s') . ' ' . $message . "\n", FILE_APPEND);
     }
 }
