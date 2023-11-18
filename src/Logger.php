3f586b692d46c5b5168d60b4fd28c751cf859d6f
Russell Purinton <russell.purinton@gmail.com>
2023-11-17 22:49:49 -0500

    Update Logger.php

--- src/Logger.php
+++ src/Logger.php
@@ -4,12 +4,19 @@ namespace RPurinton\Discommand2;
 
 class Logger
 {
+    private static $log_dir = __DIR__ . '/../logs.d';
+
     public static function log($message)
     {
-        $log_dir = __DIR__ . '/../logs.d';
-        if (!is_dir($log_dir)) mkdir($log_dir);
-        $log_file = $log_dir . '/' . date('Y-m-d') . '.log';
-        echo (date('Y-m-d H:i:s') . ' ' . $message . "\n");
-        file_put_contents($log_file, date('Y-m-d H:i:s') . ' ' . $message . "\n", FILE_APPEND);
+        if (!is_dir(self::$log_dir)) {
+            mkdir(self::$log_dir, 0777, true);
+        }
+
+        $log_file = self::$log_dir . '/' . date('Y-m-d') . '.log';
+        $timestamp = "[" . date('Y-m-d H:i:s') . '.' . explode(' ', microtime())[0] . ']';
+        $message = "$timestamp $message\n";
+
+        file_put_contents($log_file, $message, FILE_APPEND);
+        echo $message;
     }
 }
