9b23a85f920d0dbc3fccff2798c7e2fe4971797d
Russell Purinton <russell.purinton@gmail.com>
2023-11-19 01:19:02 -0500

    Update SqlException.php

--- src/Exceptions/SqlException.php
+++ src/Exceptions/SqlException.php
@@ -4,5 +4,5 @@ namespace RPurinton\Discommand2\Exceptions;
 
 class SqlException extends \Exception
 {
-    // Custom exception for logging-related errors
+    // Custom exception for SQL-related errors
 }
