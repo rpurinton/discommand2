f441e5c7b667f2417aa63fe7f996fec740d6c2ae
Russell Purinton <russell.purinton@gmail.com>
2023-11-19 03:39:02 -0500

    Update sendCrashAlert.php

--- sendCrashAlert.php
+++ sendCrashAlert.php
@@ -3,7 +3,7 @@
 // sendCrashAlert.php - Sends an email alert when a Brain service crashes
 
 $brainName = $argv[1] ?? 'unknown';
-$subject = "Discommand Brain Service Crash Alert: {$brainName}";
-$message = "The Discommand Brain service for '{$brainName}' has crashed and failed to restart. Please investigate the issue.";
+$subject = "Discommand Brain Restart: {$brainName}";
+$message = "The Discommand Brain service for '{$brainName}' has restarted.\n\n";
 
 mail('russell.purinton@gmail.com', $subject, $message, 'From: root@discommand.com');
