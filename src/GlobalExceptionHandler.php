e9c7de71e9cabb4a8007f5ae23004fa660511a69
Russell Purinton <russell.purinton@gmail.com>
2023-11-19 02:47:54 -0500

    Update GlobalExceptionHandler.php

--- src/GlobalExceptionHandler.php
+++ src/GlobalExceptionHandler.php
@@ -12,11 +12,13 @@ class GlobalExceptionHandler
 
     public function handleException(\Throwable $exception): void
     {
+        echo ("Debug: GlobalExceptionHandler::handleException() called with " . get_class($exception) . " deatils: " . $exception->getMessage() . "\n");
         $this->logger->log($exception->getMessage(), 'ERROR');
         // Signal error status for systemd
         if (trim(shell_exec('whoami') ?? "") === 'root') {
             // Use the exit code 1 to signal a general error
             // we shouldn't be running as root anyway
+            echo ("Exiting due to exeception as root.\n");
             exit(1);
         }
     }
