05e02e955684d8740e3eee2db2c4e128d5a9e537
Russell Purinton <russell.purinton@gmail.com>
2023-11-20 10:24:09 -0500

    Update RequestHandler.php

--- src/OpenAI/RequestHandler.php
+++ src/OpenAI/RequestHandler.php
@@ -3,7 +3,6 @@
 namespace RPurinton\Discommand2\OpenAI;
 
 use RPurinton\Discommand2\Core\Brain;
-use RPurinton\Discommand2\Exceptions\Exception;
 use RPurinton\Discommand2\Exceptions\FatalException;
 
 class RequestHandler
@@ -11,10 +10,10 @@ class RequestHandler
     public $promptBuilder;
     public $streamHandler;
 
-    public function __construct(private Brain $brain)
+    public function __construct(private Brain $brain, ?string $api_key = null, ?int $history_tokens = null)
     {
-        $promptBuilder = new PromptBuilder($brain);
-        $streamHandler = new StreamHandler($brain);
+        $this->promptBuilder = new PromptBuilder($brain, $api_key, $history_tokens) or throw new FatalException("Failed to initialize PromptBuilder");
+        $this->streamHandler = new StreamHandler($brain) or throw new FatalException("Failed to initialize StreamHandler");
         $this->brain->log("RequestHandler initialized.");
     }
 
