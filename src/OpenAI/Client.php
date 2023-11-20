d8d9303925cc6f1186994b1c9e8dc7429ae48ca4
Russell Purinton <russell.purinton@gmail.com>
2023-11-20 10:18:06 -0500

    Updates

--- src/OpenAI/Client.php
+++ src/OpenAI/Client.php
@@ -9,27 +9,13 @@ class Client
 {
     public $requestHandler;
     private $client;
-    private $prompt;
 
     public function __construct(private Brain $brain, $token = null)
     {
         $this->requestHandler = new RequestHandler($brain);
-        $config_file = "/home/" . $brain->myName . "/prompt.d/openai.json";
-        if (!file_exists($config_file)) throw new FatalException("OpenAI configuration file not found at $config_file", true);
-        $this->prompt = json_decode(file_get_contents($config_file), true);
-        if (!$token) $token = $this->prompt["api_key"];
-        unset($this->prompt["token"]);
-        $this->validate_token($token);
         $this->client = \OpenAI::client($token);
         if (!$this->client) throw new FatalException("Failed to initialize OpenAI client");
         $brain->log("OpenAI connected.");
         return true;
-    }
-
-    private function validate_api_key($api_key)
-    {
-        if (substr($api_key, 0, 3) !== 'sk-' || strlen($api_key) !== 51) {
-            throw new FatalException("Invalid OpenAI token");
-        }
     }
 }
--- src/OpenAI/PromptBuilder.php
+++ src/OpenAI/PromptBuilder.php
@@ -20,20 +20,20 @@ class PromptBuilder
         $this->tokenCounter = new TokenCounter($brain);
 
         $config_file = "/home/" . $brain->myName . "/prompt.d/openai.json";
-        if (!file_exists($config_file)) throw new FatalException("OpenAI configuration file not found at $config_file", true);
+        if (!file_exists($config_file)) throw new FatalException("Configuration file not found at $config_file", true);
 
         $this->prompt = json_decode(file_get_contents($config_file), true);
         if (!$this->prompt) throw new FatalException("Failed to parse OpenAI configuration file at $config_file", true);
 
         if (!$api_key) $api_key = $this->prompt["api_key"] ?? null;
         unset($this->prompt["api_key"]);
-        if (!$api_key) throw new FatalException("OpenAI API key not found in configuration file at $config_file", true);
-        if (!$this->validate_api_key($api_key)) throw new FatalException("Invalid OpenAI API key", true);
+        if (!$api_key) throw new FatalException("API key not found in configuration file at $config_file", true);
+        if (!$this->validate_api_key($api_key)) throw new FatalException("Invalid API key", true);
 
         if (!$history_tokens) $history_tokens = $this->prompt["history_tokens"] ?? null;
         unset($this->prompt["history_tokens"]);
-        if (!$history_tokens) throw new FatalException("OpenAI history_tokens not found in configuration file at $config_file", true);
-        if (!is_int($history_tokens)) throw new FatalException("Invalid OpenAI history_tokens in $config_file", true);
+        if (!$history_tokens) throw new FatalException("history_tokens not found in configuration file at $config_file", true);
+        if (!is_int($history_tokens)) throw new FatalException("Invalid history_tokens in $config_file", true);
 
         $this->brain->log("PromptBuilder initialized.");
     }
