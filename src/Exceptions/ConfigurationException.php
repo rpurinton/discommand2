35b00504160c3c043b4db255fea7f7ff56a392ad
Russell Purinton <russell.purinton@gmail.com>
2023-11-20 03:45:47 -0500

    Update ConfigurationException.php

--- src/Exceptions/ConfigurationException.php
+++ src/Exceptions/ConfigurationException.php
@@ -4,5 +4,10 @@ namespace RPurinton\Discommand2\Exceptions;
 
 class ConfigurationException extends \Exception
 {
-    // Custom exception for configuration-related errors
+    public function __construct($message = null, $code = 0, \Throwable $previous = null)
+    {
+        if (!$message) $message = "Configuration error";
+        parent::__construct($message, $code, $previous);
+        die();
+    }
 }
