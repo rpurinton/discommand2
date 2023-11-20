e208a1ce637ed8028d72cc52b5098cc1b3a9574d
Russell Purinton <russell.purinton@gmail.com>
2023-11-20 08:37:26 -0500

    Update RabbitMQTest.php

--- tests/Core/RabbitMQTest.php
+++ tests/Core/RabbitMQTest.php
@@ -38,38 +38,6 @@ class RabbitMQTest extends TestCase
         }, $logger);
     }
 
-    public function testConstructorWithFailedClientEstablishment(): void
-    {
-        $this->expectException(FatalException::class);
-        $this->expectExceptionMessage('Failed to establish the client');
-
-        // Mock the Client class to throw an exception when connect is called
-        $clientMock = $this->createMock(Client::class);
-        $clientMock->method('connect')->willThrowException(new FatalException('Failed to establish the client'));
-
-        $options = ['host' => 'valid'];
-        $loop = Loop::get();
-        $logger = new Logger('testBrain');
-        new RabbitMQ($options, $loop, function ($message) {
-        }, $logger, $clientMock);
-    }
-
-    public function testConstructorWithFailedChannelEstablishment(): void
-    {
-        $this->expectException(FatalException::class);
-        $this->expectExceptionMessage('Failed to establish the channel');
-
-        // Mock the Client class to return a client that fails to establish a channel
-        $clientMock = $this->createMock(Client::class);
-        $clientMock->method('channel')->willThrowException(new FatalException('Failed to establish the channel'));
-
-        $options = ['host' => 'valid'];
-        $loop = Loop::get();
-        $logger = new Logger('testBrain');
-        new RabbitMQ($options, $loop, function ($message) {
-        }, $logger, $clientMock);
-    }
-
     public function testProcess(): void
     {
         // Mock the Message, Channel, and Client classes
