c8e2ebd64454a52287619bb8bbd0df69acab7a9a
Russell Purinton <russell.purinton@gmail.com>
2023-11-19 21:20:35 -0500

    Update Logger.php

--- src/Core/Logger.php
+++ src/Core/Logger.php
@@ -6,10 +6,12 @@ use RPurinton\Discommand2\Exceptions\LogException;
 
 class Logger
 {
+    private float $boot_microtime = 0;
     private float $last_microttime = 0;
 
     public function __construct(private string $log_dir)
     {
+        $this->boot_microtime = microtime(true);
         $this->last_microttime = microtime(true);
         // Simulate a failure condition for testing purposes
         if ($log_dir === '/root/invalid/log/dir') {
@@ -25,12 +27,18 @@ class Logger
     public function log($message, $level = 'INFO')
     {
         $microtime = microtime(true);
-        $diff = $microtime - $this->last_microttime;
-        $this->last_microttime = $microtime;
-        $diff = number_format($diff, 6, '.', '') . 's';
+
+        $boot_diff = number_format($microtime - $this->boot_microtime, 6, '.', '') . 's';
+        while (strlen($boot_diff) < 14) $boot_diff = '0' . $boot_diff;
+
+        $diff = number_format($microtime - $this->last_microttime, 6, '.', '') . 's';
         while (strlen($diff) < 14) $diff = '0' . $diff;
+        $this->last_microttime = $microtime;
+
         $log_file = $this->log_dir . '/' . date('Y-m-d') . '.log';
-        $log_message = "[" . date('Y-m-d H:i:s') . '.' . substr(number_format(microtime(true), 6, '.', ''), -6) . "] ($diff) [$level] $message\n";
+        $log_message = "[" . date('Y-m-d H:i:s') . '.' . substr(number_format(microtime(true), 6, '.', ''), -6) . "] ($boot_diff:$diff) [$level] $message\n";
+
+        file_put_contents($log_file, $log_message, FILE_APPEND | LOCK_EX) or throw new LogException("Failed to write to log file: {$log_file}");
 
         $level = strtoupper($level);
         $syslogPriority = match ($level) {
